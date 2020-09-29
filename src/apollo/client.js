a3fd5b90b01e9cf28aab7d51c603db1820d34fdb
hanyunx <hanyunx2@gmail.com>
2020-09-28 18:17:14 -0700

    Update links, client, factory addr for Equalizer

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/ianlapham/uniswapv2'
+    uri: 'https://api.thegraph.com/subgraphs/name/hanyunx/equalizer-subgraph-fork'
   }),
   cache: new InMemoryCache(),
   shouldBatch: true
